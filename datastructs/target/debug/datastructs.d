765092aeb577676bb037e43a365a4aa33c0c714b
ReyLegar <rlegar@mail.ru>
2023-11-19 17:27:07 +0300

    Add data structs lesson project

---
+++ datastructs/target/debug/datastructs.d
@@ -0,0 +1,1 @@
+C:\Users\ReyLegar\Desktop\computer_science_lessons\datastructs\target\debug\datastructs.exe: C:\Users\ReyLegar\Desktop\computer_science_lessons\datastructs\src\main.rs
---
+++ datastructs/target/debug/deps/datastructs.d
@@ -0,0 +1,5 @@
+C:\Users\ReyLegar\Desktop\computer_science_lessons\datastructs\target\debug\deps\datastructs.exe: src\main.rs
+
+C:\Users\ReyLegar\Desktop\computer_science_lessons\datastructs\target\debug\deps\datastructs.d: src\main.rs
+
+src\main.rs:
